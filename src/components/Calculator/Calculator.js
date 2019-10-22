eddd9b21cdc7765365c6c1f3d77b42c9b67ea881
bhavanapanduru <p.bhavana1997@gmail.com>
2019-10-22 15:45:13 +0530

    Create Calculator component and load it in our app

---
+++ src/components/Calculator/Calculator.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Calculator extends React.Component {
+    render() {
+        return <div>Calculator Component Works!</div>;
+    }
+}
+
+export default Calculator;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
+import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+
+import Calculator from "./components/Calculator/Calculator";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(null, document.getElementById("root"));
+ReactDOM.render(<Calculator />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
