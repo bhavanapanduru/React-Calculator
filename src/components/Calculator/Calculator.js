96cef39c0d2489e837bfe250119c7070d18d7dd9
bhavanapanduru <p.bhavana1997@gmail.com>
2019-10-23 15:34:10 +0530

    Add button component and create buttons and text area for calculator

--- src/components/Calculator/Calculator.js
+++ src/components/Calculator/Calculator.js
@@ -1,8 +1,26 @@
 import React from "react";
+import "./calculator.css";
+import Button from "../Button/Button";
 
 class Calculator extends React.Component {
     render() {
-        return <div>Calculator Component Works!</div>;
+        return (
+            <>
+                <textarea></textarea>
+                <Button value={0} />
+                <Button value={1} />
+                <Button value={2} />
+                <Button value={3} />
+                <Button value={4} />
+                <Button value={5} />
+                <Button value={6} />
+                <Button value={7} />
+                <Button value={8} />
+                <Button value={9} />
+                <Button value={"+"} />
+                <Button value={"-"} />
+            </>
+        );
     }
 }
 
