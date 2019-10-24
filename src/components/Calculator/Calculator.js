ee243a5ae097ecce39f29f12da13730e1192aa1a
bhavanapanduru <p.bhavana1997@gmail.com>
2019-10-24 12:27:50 +0530

    Remove Button as it is again using default button

--- src/components/Calculator/Calculator.js
+++ src/components/Calculator/Calculator.js
@@ -1,26 +1,9 @@
 import React from "react";
 import "./calculator.css";
-import Button from "../Button/Button";
 
 class Calculator extends React.Component {
     render() {
-        return (
-            <>
-                <textarea></textarea>
-                <Button value={0} />
-                <Button value={1} />
-                <Button value={2} />
-                <Button value={3} />
-                <Button value={4} />
-                <Button value={5} />
-                <Button value={6} />
-                <Button value={7} />
-                <Button value={8} />
-                <Button value={9} />
-                <Button value={"+"} />
-                <Button value={"-"} />
-            </>
-        );
+        return <div>hello</div>;
     }
 }
 
