b0c0918dd27876e285b290d55202e16d60b6e405
bhavanapanduru <p.bhavana1997@gmail.com>
2019-10-31 12:08:05 +0530

    Remove bold property for the output that will be shown in calculator

--- src/components/Result/Result.js
+++ src/components/Result/Result.js
@@ -2,11 +2,7 @@ import React from "react";
 import "./Result.css";
 
 function Result(props) {
-    return (
-        <p id="value">
-            <strong>{props.value}</strong>
-        </p>
-    );
+    return <p id="value">{props.value}</p>;
 }
 
 export default Result;
